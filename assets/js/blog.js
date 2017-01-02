ff29d9eb0f49e359bc14738a83ea50452985178f
Colson Donohue <colsondonohue@gmail.com>
2017-01-02 18:15:02 -0400

    Simplify blog code and fix loading bug on polyfilled browsers

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,47 +5,18 @@
 
   simpla.get('blog').then(function(postIDs) {
     blogCount = postIDs.length || 0;
-    var countDown = blogCount;
-    if (postIDs.constructor === Array) {
-      (function render(index) {
-        var post,
-            postChecker = true;
-
-        simpla.get('blog.' + postIDs[index] + '.post').then(function(postText) {
-          if (postText.text == '<p><br></p>') {
-            postChecker = false;
-          }
-        }).then(function() {
-          if (index >= blogCount - 5 && postChecker) {
-            post = document.createElement('simpla-block');
-
-            post.sid = postIDs[index];
-            post.innerHTML += '<figure class="info-page__image info-page__image--half">' +
-                                '<simpla-img class="info-page__figure-image" sid="image-1"></simpla-img>' +
-                                '<figcaption><simpla-text sid="caption-1"></simpla-text></figcaption>' +
-                              '</figure>';
-            if (postIDs[index]['image-2']) {
-              post.innerHTML += '<figure class="info-page__image info-page__image--half">' +
-                                  '<simpla-img class="info-page__figure-image" sid="image-2"></simpla-img>' +
-                                  '<figcaption><simpla-text sid="caption-2"></simpla-text></figcaption>' +
-                                '</figure>';
-            }
-            post.innerHTML += '<h3 class="info-page__subtitle info-page__subtitle--no-top"><simpla-text sid="title"></simpla-text></h3>' +
-                              '<simpla-text sid="post"></simpla-text>';
-
-            container.insertBefore(post, container.firstChild);
-          }
-        }).then(function() {
-          countDown--;
-          if (countDown !== 0) {
-            render(index + 1);
-          }
-        });
-      }(0));
+    for (var i = 1; i < blogCount ; i++) {
+      post = document.createElement('simpla-block');
+      post.sid = 'blog-' + i;
+      post.innerHTML += '<figure class="info-page__image info-page__image--half">' +
+                          '<simpla-img class="info-page__figure-image" sid="image-1"></simpla-img>' +
+                          '<figcaption><simpla-text sid="caption-1"></simpla-text></figcaption>' +
+                        '</figure>';
+      post.innerHTML += '<h3 class="info-page__subtitle info-page__subtitle--no-top"><simpla-text sid="title"></simpla-text></h3>' +
+                        '<simpla-text sid="post"></simpla-text>';
+      container.insertBefore(post, container.firstChild);
     }
-  });
-
-  (function detectEdit() {
+  }).then((function detectEdit() {
     var url = window.location.href,
         newPost;
     if (url.indexOf('#edit') != -1) {
@@ -55,10 +26,6 @@
                             '<simpla-img class="info-page__figure-image" sid="image-1"></simpla-img>' +
                             '<figcaption><simpla-text sid="caption-1"></simpla-text></figcaption>' +
                           '</figure>' +
-                          '<figure class="info-page__image info-page__image--half">' +
-                            '<simpla-img class="info-page__figure-image" sid="image-2"></simpla-img>' +
-                            '<figcaption><simpla-text sid="caption-2"></simpla-text></figcaption>' +
-                          '</figure>' +
                           '<h3 class="info-page__subtitle info-page__subtitle--no-top"><simpla-text sid="title"></simpla-text></h3>' +
                           '<simpla-text sid="post"></simpla-text>';
 
@@ -67,5 +34,5 @@
     else {
       setTimeout(detectEdit, 500);
     }
-  }());
+  }));
 }());
